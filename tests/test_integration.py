f5417f0b2a1e906b27cb3486655b5cf2054ae4bb
Tommy-Dan <ttuckerwra@gmail.com>
2024-08-30 17:31:46 +0000

    commit changes

---
+++ tests/test_integration.py
@@ -0,0 +1,22 @@
+#Import testing Liberies
+import unittest
+from onlinebanking_system.banking import BankingSystem
+
+
+#This is the test-case class
+class TestBankingSystem(unittest.TestCase):
+
+    def setUp(self):
+        self.bank = BankingSystem()
+        
+def test_integration_workflow(self):
+    self.bank.register_user("team@user", "team@123")
+    self.bank.register_user("user@team", "5678")
+    self.bank.login("team@user", "team@123")
+    self.bank.deposit(100)
+    self.bank.transfer("user@team", 50)
+    self.assertEqual(self.bank.users["team@user"]["balance"], 50)
+    self.assertEqual(self.bank.users["team@user"]["balance"], 50)
+
+if __name__ == "__main__":
+    test_integration_workflow()
